2f7fa1105a8ee6c8efb6fd8b0f3a89e9c8390e40
Sowmay Jain <sowmayjn@gmail.com>
2020-06-10 19:37:54 +1000

    removed Candra

--- contracts/deployer.sol
+++ contracts/deployer.sol
@@ -5,7 +5,7 @@ interface IERC20 {
   function transfer(address, uint) external returns (bool);
 }
 
-contract CandraWallet {
+contract Wallet {
 
   function flush(address token) public {
     address payable treasury = 0x1d29756e8f7b091cE6c11a35980dE79c7eDa5D1f;
@@ -15,10 +15,10 @@ contract CandraWallet {
 
 }
 
-contract CandraDeployer {
+contract Deployer {
 
   function calculateAddress(uint256 seed) public view returns (address) {
-    bytes32 codeHash = keccak256(type(CandraWallet).creationCode);
+    bytes32 codeHash = keccak256(type(Wallet).creationCode);
     bytes32 salt = bytes32(seed);
     bytes32 rawAddress = keccak256(
       abi.encodePacked(
@@ -32,13 +32,13 @@ contract CandraDeployer {
   }
 
   function deploy(uint256 seed, address token) public returns (address wallet) {
-    bytes memory code = type(CandraWallet).creationCode;
+    bytes memory code = type(Wallet).creationCode;
     bytes32 salt = bytes32(seed);
     assembly {
         wallet := create2(0, add(code, 0x20), mload(code), salt)
         if iszero(extcodesize(wallet)) {revert(0, 0)}
     }
-    CandraWallet(wallet).flush(token);
+    Wallet(wallet).flush(token);
   }
 
 }
